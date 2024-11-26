3ec9bb4a5b4b15c9b2eb3eea270fcf24a5ba65de
Lopez Sebastian <sebastian.lopez.mdq@gmail.com>
2024-11-26 16:49:09 -0300

    Se terminaron los componentes
    Se completaron los componentes de sobre mi y contacto

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-page.component.html',
   styleUrl: './form-page.component.css'
 })
 export class FormPageComponent {
-
+  
+  
+  
+  
 }
