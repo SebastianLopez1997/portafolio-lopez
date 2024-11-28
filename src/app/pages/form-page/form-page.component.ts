362f394237c42b847088043e407623570ec6dc8e
Lopez Sebastian <sebastian.lopez.mdq@gmail.com>
2024-11-28 17:26:20 -0300

    Validadores del Reactive form

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-page',
@@ -10,7 +10,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class FormPageComponent {
   
+  fb = inject(FormBuilder)
   
+  formulario = this.fb.nonNullable.group({
+    name: ['',[Validators.required,Validators.minLength(3)]],
+    email: ['',[Validators.required,Validators.email]],
+    subject: ['',[Validators.required,Validators.minLength(4)]],
+    message: ['Hola! me gustaria contratar tus servicios',[Validators.required,Validators.minLength(15)]]
+  })
+  
+  formOk(){
+    if(this.formulario.invalid) return
+    
+    
+  }
   
   
 }
